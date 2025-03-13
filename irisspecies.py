a454820532a7f3907bf69c2258f4fee2e79a8caf
Krishnakumar V <krishna.v15122004@gmail.com>
2025-03-13 15:33:36 +0530

    Update irisspecies.py

--- irisspecies.py
+++ irisspecies.py
@@ -33,7 +33,7 @@ def predict_species(sl,sw,pl,pw):
 
 if __name__ == "__main__":
     st.title('Iris Species prediction with Decision Tree model By Krish')
-    st.image('IRIS.png')
+    st.image('iris.png')
 
     sepal_length = get_sepal_length()
     sepal_width = get_sepal_width()
